80b5324e28f5c403446c0e3f8869629e73ec42b2
Shanmathi Thamizhinian <122631445+Shanmathi-Thamizhinian@users.noreply.github.com>
2024-03-20 17:32:03 +0530

    Create d1q1.cpp

---
+++ d1q1.cpp
@@ -0,0 +1,33 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode() : val(0), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+ *     TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left), right(right) {}
+ * };
+ */
+class Solution {
+public:
+    vector<vector<int>> levelOrder(TreeNode* root) {
+        vector<vector<int>> ans;
+        queue<TreeNode*> q;
+        if(root==NULL) return ans;
+        q.push(root);
+        while(!q.empty()){
+            int n=q.size();
+            vector<int> temp;
+            for(int i=0;i<n;i++){
+                TreeNode* key=q.front();
+                q.pop();
+                if(key->left!=NULL) q.push(key->left);
+                if(key->right!=NULL) q.push(key->right);
+                temp.push_back(key->val);
+            }
+            ans.push_back(temp);
+        }
+        return ans;
+    }
+};
